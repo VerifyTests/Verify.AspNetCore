258a2ec5a281414e6db953e4acc01d0b6495fff2
Simon Cropp <simon.cropp@gmail.com>
2021-10-01 16:06:45 +1000

    .

--- src/Verify.AspNetCore/Extensions.cs
+++ src/Verify.AspNetCore/Extensions.cs
@@ -1,25 +1,12 @@
 ﻿using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Net.Http.Headers;
 
 static class Extensions
 {
-    public static bool IsText(this HttpContent content)
-    {
-        var contentType = content.Headers.ContentType;
-        if (contentType?.MediaType == null)
-        {
-            return false;
-        }
-
-        return contentType.MediaType.StartsWith("text");
-    }
-
     public static void MoveToStart(this Stream stream)
     {
         if (stream.CanSeek)
@@ -33,12 +20,6 @@ static class Extensions
         stream.MoveToStart();
         using StreamReader reader = new(stream);
         return await reader.ReadToEndAsync();
-    }
-
-    public static Dictionary<string, string> ToDictionary(this HttpHeaders headers)
-    {
-        return headers
-            .ToDictionary(x => x.Key, x => string.Join('|', x.Value));
     }
 
     public static Dictionary<string, string> NotCookies(this IHeaderDictionary headers)
