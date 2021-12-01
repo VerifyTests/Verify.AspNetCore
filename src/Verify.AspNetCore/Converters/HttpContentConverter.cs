6dd88c1e8d9d014f7c980f817198f56917da01de
David Gardiner <david@gardiner.net.au>
2021-12-01 20:02:23 +1030

    Support converting HttpContent
    Implements #2

---
+++ src/Verify.AspNetCore/Converters/HttpContentConverter.cs
@@ -0,0 +1,30 @@
+﻿using System.Net.Http;
+using Newtonsoft.Json;
+using VerifyTests;
+
+public class HttpContentConverter : WriteOnlyJsonConverter<HttpContent>
+{
+    public override void WriteJson(JsonWriter writer, HttpContent content, JsonSerializer serializer, IReadOnlyDictionary<string, object> context)
+    {
+        writer.WriteStartObject();
+
+        WriteHeaders(writer, serializer, content);
+
+        writer.WritePropertyName("Body");
+        serializer.Serialize(writer, content.ReadAsStringAsync().Result);
+
+        writer.WriteEndObject();
+    }
+
+    static void WriteHeaders(JsonWriter writer, JsonSerializer serializer, HttpContent content)
+    {
+        var headers = content.Headers;
+        if (!headers.Any())
+        {
+            return;
+        }
+
+        writer.WritePropertyName("Headers");
+        serializer.Serialize(writer, headers);
+    }
+}
