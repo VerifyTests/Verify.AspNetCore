e5dacc4c2dc7986f659a5cd401bd35b03d5ea507
Simon Cropp <simon.cropp@gmail.com>
2022-06-15 13:25:27 +1000

    .

---
+++ src/Verify.AspNetCore/SelectedControlers/ControllerProvider.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.Controllers;
+
+class ControllerProvider :
+    ControllerFeatureProvider
+{
+    Type[] controllers;
+
+    public ControllerProvider(Type[] controllers) =>
+        this.controllers = controllers;
+
+    protected override bool IsController(TypeInfo type) =>
+        controllers.Contains(type);
+}
