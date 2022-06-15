5a3a8a2c6b5f0be3fa67a3b38190bf2c8d9cd7cd
Simon Cropp <simon.cropp@gmail.com>
2022-06-15 13:29:22 +1000

    cleanup

--- src/Verify.AspNetCore/FileResultInfo.cs
+++ src/Verify.AspNetCore/FileResultInfo.cs
@@ -1,10 +1,8 @@
 ﻿using Microsoft.Net.Http.Headers;
 
-class FileResultInfo
-{
-    public string FileDownloadName { get; set; } = null!;
-    public DateTimeOffset? LastModified { get; set; }
-    public EntityTagHeaderValue? EntityTag { get; set; }
-    public bool EnableRangeProcessing { get; set; }
-    public string ContentType { get; set; } = null!;
-}
+record FileResultInfo(
+    string FileDownloadName,
+    DateTimeOffset? LastModified,
+    EntityTagHeaderValue? EntityTag,
+    bool EnableRangeProcessing,
+    string ContentType);
