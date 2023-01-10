76e4504a1ac894480f197254afc827001f31147e
Simon Cropp <simon.cropp@gmail.com>
2023-01-10 21:43:30 +1100

    .

--- src/RazorPageWebApplication/Program.cs
+++ src/RazorPageWebApplication/Program.cs
@@ -1,15 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-}
 app.UseStaticFiles();
 
 app.UseRouting();
