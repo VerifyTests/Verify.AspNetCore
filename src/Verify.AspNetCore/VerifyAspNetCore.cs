44763c5ca7f30bea38c088afe3b24475a2c4fde5
Simon Cropp <simon.cropp@gmail.com>
2023-09-11 11:13:15 +1000

    Verify 21.1

--- src/Verify.AspNetCore/VerifyAspNetCore.cs
+++ src/Verify.AspNetCore/VerifyAspNetCore.cs
@@ -1,4 +1,5 @@
-﻿using EmptyFiles;
+﻿using Argon;
+using EmptyFiles;
 using Microsoft.AspNetCore.Mvc;
 
 namespace VerifyTests;
@@ -6,10 +7,6 @@ namespace VerifyTests;
 public static partial class VerifyAspNetCore
 {
     public static bool Initialized { get; private set; }
-
-    [Obsolete("Use Initialize()")]
-    public static void Enable() =>
-        Initialize();
 
     public static void Initialize()
     {
@@ -25,59 +22,56 @@ public static partial class VerifyAspNetCore
         VerifierSettings.RegisterFileConverter<FileContentResult>(ConvertFileResult);
         VerifierSettings.RegisterFileConverter<PhysicalFileResult>(ConvertFileResult);
         VerifierSettings.RegisterFileConverter<VirtualFileResult>(ConvertFileResult);
-        VerifierSettings.AddExtraSettings(serializer =>
-        {
-            var converters = serializer.Converters;
-            converters.Add(new HttpContextConverter());
-            converters.Add(new HttpResponseConverter());
-            converters.Add(new HeaderDictionaryConverter());
-            converters.Add(new HttpRequestConverter());
-            converters.Add(new ChallengeResultConverter());
-            converters.Add(new ActionResultConverter());
-            converters.Add(new ContentResultConverter());
-            converters.Add(new ControllerContextConverter());
-            converters.Add(new EmptyResultConverter());
-            converters.Add(new FileContentResultConverter());
-            converters.Add(new FileStreamResultConverter());
-            converters.Add(new PhysicalFileResultConverter());
-            converters.Add(new VirtualFileResultConverter());
-            converters.Add(new ForbidResultConverter());
-            converters.Add(new JsonResultConverter());
-            converters.Add(new LocalRedirectResultConverter());
-            converters.Add(new ObjectResultConverter());
-            converters.Add(new AcceptedAtActionResultConverter());
-            converters.Add(new AcceptedAtRouteResultConverter());
-            converters.Add(new AcceptedResultConverter());
-            converters.Add(new BadRequestObjectResultConverter());
-            converters.Add(new ConflictObjectResultConverter());
-            converters.Add(new CreatedAtActionResultConverter());
-            converters.Add(new CreatedAtRouteResultConverter());
-            converters.Add(new CreatedResultConverter());
-            converters.Add(new NotFoundObjectResultConverter());
-            converters.Add(new OkObjectResultConverter());
-            converters.Add(new UnauthorizedObjectResultConverter());
-            converters.Add(new UnprocessableEntityObjectResultConverter());
-            converters.Add(new PartialViewResultConverter());
-            converters.Add(new RedirectResultConverter());
-            converters.Add(new RedirectToActionResultConverter());
-            converters.Add(new RedirectToPageResultConverter());
-            converters.Add(new RedirectToRouteResultConverter());
-            converters.Add(new SignInResultConverter());
-            converters.Add(new SignOutResultConverter());
-            converters.Add(new StatusCodeResultConverter());
-            converters.Add(new BadRequestResultConverter());
-            converters.Add(new ConflictResultConverter());
-            converters.Add(new NoContentResultConverter());
-            converters.Add(new NotFoundResultConverter());
-            converters.Add(new OkResultConverter());
-            converters.Add(new UnauthorizedResultConverter());
-            converters.Add(new UnprocessableEntityResultConverter());
-            converters.Add(new UnsupportedMediaTypeResultConverter());
-            converters.Add(new ConflictResultConverter());
-            converters.Add(new ViewComponentResultConverter());
-            converters.Add(new ViewResultConverter());
-            converters.Add(new PageResultConverter());
-        });
+        var converters = DefaultContractResolver.Converters;
+        converters.Add(new HttpContextConverter());
+        converters.Add(new HttpResponseConverter());
+        converters.Add(new HeaderDictionaryConverter());
+        converters.Add(new HttpRequestConverter());
+        converters.Add(new ChallengeResultConverter());
+        converters.Add(new ActionResultConverter());
+        converters.Add(new ContentResultConverter());
+        converters.Add(new ControllerContextConverter());
+        converters.Add(new EmptyResultConverter());
+        converters.Add(new FileContentResultConverter());
+        converters.Add(new FileStreamResultConverter());
+        converters.Add(new PhysicalFileResultConverter());
+        converters.Add(new VirtualFileResultConverter());
+        converters.Add(new ForbidResultConverter());
+        converters.Add(new JsonResultConverter());
+        converters.Add(new LocalRedirectResultConverter());
+        converters.Add(new ObjectResultConverter());
+        converters.Add(new AcceptedAtActionResultConverter());
+        converters.Add(new AcceptedAtRouteResultConverter());
+        converters.Add(new AcceptedResultConverter());
+        converters.Add(new BadRequestObjectResultConverter());
+        converters.Add(new ConflictObjectResultConverter());
+        converters.Add(new CreatedAtActionResultConverter());
+        converters.Add(new CreatedAtRouteResultConverter());
+        converters.Add(new CreatedResultConverter());
+        converters.Add(new NotFoundObjectResultConverter());
+        converters.Add(new OkObjectResultConverter());
+        converters.Add(new UnauthorizedObjectResultConverter());
+        converters.Add(new UnprocessableEntityObjectResultConverter());
+        converters.Add(new PartialViewResultConverter());
+        converters.Add(new RedirectResultConverter());
+        converters.Add(new RedirectToActionResultConverter());
+        converters.Add(new RedirectToPageResultConverter());
+        converters.Add(new RedirectToRouteResultConverter());
+        converters.Add(new SignInResultConverter());
+        converters.Add(new SignOutResultConverter());
+        converters.Add(new StatusCodeResultConverter());
+        converters.Add(new BadRequestResultConverter());
+        converters.Add(new ConflictResultConverter());
+        converters.Add(new NoContentResultConverter());
+        converters.Add(new NotFoundResultConverter());
+        converters.Add(new OkResultConverter());
+        converters.Add(new UnauthorizedResultConverter());
+        converters.Add(new UnprocessableEntityResultConverter());
+        converters.Add(new UnsupportedMediaTypeResultConverter());
+        converters.Add(new ConflictResultConverter());
+        converters.Add(new ViewComponentResultConverter());
+        converters.Add(new ViewResultConverter());
+        converters.Add(new PageResultConverter());
     }
 
     static ConversionResult ConvertFileResult(FileContentResult target, IReadOnlyDictionary<string, object> context)
