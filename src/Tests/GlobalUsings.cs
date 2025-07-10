160a27a4f707dcedde6a7479fb5351ac9c6e4f3d
Simon Cropp <simon.cropp@gmail.com>
2025-07-10 12:02:03 +1000

    refs or cleanup

--- src/Tests/GlobalUsings.cs
+++ src/Tests/GlobalUsings.cs
@@ -1,5 +1,3 @@
-﻿// Global using directives
-
-global using Microsoft.AspNetCore.Mvc;
+﻿global using Microsoft.AspNetCore.Mvc;
 global using Microsoft.AspNetCore.Mvc.Testing;
 global using NUnit.Framework.Legacy;
